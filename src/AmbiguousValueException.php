6b7ad2306f69f9e8784b009764091ea6a8fc7f46
Taner Kucukyuruk <hello@taner.work>
2022-05-18 22:33:35 +0200

    sort matches in ascending order

--- src/AmbiguousValueException.php
+++ src/AmbiguousValueException.php
@@ -4,12 +4,17 @@ namespace Kata;
 
 class AmbiguousValueException extends \Exception
 {
+    private array $matches;
+
     public function __construct(
         string $message,
-        private readonly array $matches,
+        array $matches = [],
         int $code = 0,
         \Throwable|null $previous = null
     ) {
+        sort($matches);
+        $this->matches = $matches;
+
         parent::__construct($message, $code, $previous);
     }
 
